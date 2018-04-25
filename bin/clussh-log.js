b77fe3fcfe699f99cb5664b58486b90b612f7ed0
Jean Ponchon <jean.ponchon@novadiscovery.com>
2018-04-25 09:38:10 +0200

    feat: Enhance clussh-log output

--- bin/clussh-log.js
+++ bin/clussh-log.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 const clusshLog = require('../lib/clussh-log.js')
+const yargs = require('yargs')
 
 process.stdin
-  .pipe(clusshLog())
+  .pipe(clusshLog(yargs.parse()))
   .pipe(process.stdout)
